69eea0f1115ee9388f7bcac0010dca6768d51318
Negasi-Kifle <kiflenegasi@yahoo.com>
2023-04-07 22:09:59 -0700

    Simple function that adds numbers

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Code will appear here...");
+function add(n1, n2, showResult) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log("Result is: ".concat(result));
+    }
+    else {
+        return result;
+    }
+}
+var num1 = 5;
+var num2 = 2.8;
+var showResult = true;
+add(num1, num2, showResult);
--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log("Code will appear here...")
+function add(n1: number, n2: number, showResult: boolean){
+    const result = n1 + n2;
+    if(showResult){
+        console.log(`Result is: ${result}`)
+    }else{
+        return result
+    }
+}
+
+const num1 = 5;
+const num2 = 2.8;
+const showResult = true
+
+add(num1, num2, showResult)
