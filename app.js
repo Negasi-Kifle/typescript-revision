86e374c75267ab6ca2a719e15b3c4f73d9f4c2f7
Negasi-Kifle <kiflenegasi@yahoo.com>
2023-04-10 11:07:16 +0300

    Enum values

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
+// Enums
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 // Object type
+// const person: {name: string, age: number, hobbies: string[], role: [string, number]} = {
 var person = {
-    // const person = {
     name: "Negasi",
     age: 27,
     hobbies: ["Cooking", "Sport"],
-    role: ["Admin", 12]
+    role: Role.ADMIN
 };
-person.role[1] = 85;
 console.log(person.role);
-console.log(person.hobbies);
+// console.log("Admin index: ", Role.ADMIN)
+// console.log(person.hobbies)
 // Declare an arry that stores string 
 var favFood;
 favFood = ['Mendi'];
-console.log(favFood);
-// Loop through all hobbies and print them all
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
-}
+// console.log(favFood)
 // A function with object type as a parameter
 // function greet (person: {name: string, age: number}){
 //     console.log(`Hello, ${person.name}`)
--- app.ts
+++ app.ts
@@ -1,25 +1,35 @@
+// Enums
+enum Role {ADMIN, READ_ONLY, AUTHOR}
+
+// Assign the first member and get other members assigned automaticaly
+enum Role2 {ADMIN=9, READ_ONLY, AUTHOR}
+
+// Assign random number to random memeber
+enum Role3 {ADMIN, READ_ONLY=7, AUTHOR}
+
+// Assign any random number to the members
+enum Role4 {ADMIN=23, READ_ONLY=12, AUTHOR=45}
+
+// Assign any value to memebers
+enum Role5 {ADMIN="ADMIN", READ_ONLY=6, AUTHOR="AUTHOR"}
+
 // Object type
 // const person: {name: string, age: number, hobbies: string[], role: [string, number]} = {
     const person = {
     name: "Negasi",
     age: 27,
     hobbies: ["Cooking", "Sport"],
-    role: ["Admin", 12]
+    role: Role.ADMIN
 }
 
-person.role[0] = 85;
 console.log(person.role)
-console.log(person.hobbies)
+// console.log("Admin index: ", Role.ADMIN)
+// console.log(person.hobbies)
 
 // Declare an arry that stores string 
 let favFood: string[];
 favFood = ['Mendi']
-console.log(favFood)
-
-// Loop through all hobbies and print them all
-for(const hobby of person.hobbies){
-    console.log(hobby)
-}
+// console.log(favFood)
 
 // A function with object type as a parameter
 // function greet (person: {name: string, age: number}){
